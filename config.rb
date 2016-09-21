269d8d5e609fcafe016d5ab28802b7cb9bfa44bc
Yoshinori Kobayashi <yoshiiikoba@gmail.com>
2016-09-21 12:17:24 +0900

    Bootstrap add

---
+++ config.rb
@@ -0,0 +1,13 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "images"
+javascripts_dir = "js"
+
+#output_style = :expanded
+output_style = :compressed
+
+line_comments = false
+
+#追加
+relative_assets = true
